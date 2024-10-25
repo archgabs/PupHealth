17b7514ede25b99aca15030981dbd5b92ab6e4fa
Gabriel <107143364+archgabs@users.noreply.github.com>
2024-10-25 15:21:25 -0300

    File: commits iniciais relativos a construção de UI

---
+++ RenderApplication.py
@@ -0,0 +1,48 @@
+import customtkinter
+from functools import partial as p 
+from DatabaseManager import login_validation
+
+
+class RenderApplication:
+    def __init__(self) -> None:
+        self.app = customtkinter.CTk()
+        customtkinter.set_appearance_mode('dark')
+        customtkinter.set_default_color_theme('green')
+
+        self.app.geometry("450x500")
+        self.app.title("Login")
+        self.app.resizable(False, False)
+        self.app.columnconfigure(0, weight=1)
+        
+        # Title
+        self.label = customtkinter.CTkLabel(master=self.app, text="Pup Health", font=("Pacifico", 48),  padx = 20, pady = 20)
+        self.label.grid(row = 0, column = 0, sticky="")
+        
+        # Login Field
+        self.login_field = customtkinter.CTkFrame(master=self.app, corner_radius=10, fg_color="#333")
+        self.login_field.grid(row = 1, column = 0, padx = 20, pady = 20)
+
+        # Login & Password 
+        self.login_input = customtkinter.CTkEntry(master=self.login_field, placeholder_text="Username", width=250, height=30)
+        self.password_input = customtkinter.CTkEntry(master=self.login_field, placeholder_text="Password", show="*", width=250, height=30)
+        
+        self.login_input.grid(row = 0, sticky = "", padx=20, pady=20)
+        self.password_input.grid(row = 1, sticky = "", padx=20, pady=(0,10))
+
+        self.forgot_password = customtkinter.CTkLabel(master=self.login_field, text="Esqueceu a senha / Primeiro acesso?", font=("arial", 12))
+        self.forgot_password.grid(row=2, sticky="w", padx=20, pady=(0,20))
+
+        # Login Button
+        self.login_button = customtkinter.CTkButton(master=self.app, text="Connect", font=("Arial", 16), width=280,
+                                                    command=p(self.send_validation),)
+        self.login_button.grid(row=2, column=0, padx=20, pady=20, sticky = '')
+        
+        self.app.mainloop()
+
+
+    def send_validation(self) -> None:
+        lg = self.login_input.get() 
+        ps = self.password_input.get()
+        print(login_validation(lg, ps))
+        
+        
---
+++ main.py
@@ -0,0 +1,5 @@
+from RenderApplication import RenderApplication
+
+
+if __name__ == '__main__':
+    render = RenderApplication()
