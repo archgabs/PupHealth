1009c7a21625a8475050e3f05611592c314403bf
Gabriel <107143364+archgabs@users.noreply.github.com>
2024-10-29 15:49:16 -0300

    Update: Finalizado procedimento de alteração de senha.

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -8,14 +8,13 @@ valid_user = r'^[0-9A-Za-z]{6,16}$'
 valid_pass = r'^(?=.*?[0-9])(?=.*?[A-Za-z]).{8,32}$' 
   
 
-def login_validation(username: str = False, password: str = False) -> str:
-    
+def login_validation(username: str = False, password: str = False) -> str:  
     if re.match(valid_user, username) and re.match(valid_pass, password):
         try:
             handler = sqlite3.connect('database/database.db')
             cursor = handler.cursor()
-        except:
-            print("Connection to DB failed.")
+        except Exception as e:
+            print("Connection to DB failed.", e)
             return "FAILED"
         else:
                 users = cursor.execute("SELECT user, password FROM users").fetchall()
@@ -27,18 +26,18 @@ def login_validation(username: str = False, password: str = False) -> str:
         return "NOT VALID"
 
    
-def get_user_email(username: str) -> str:
+def get_user_email(username: str):
     """Verifica se o usuário existe e retorna o e-mail associado."""
     if not re.match(valid_user, username):
         print("Username não é válido.")
-        return False  # Retorna None se o username não for válido
-
+        return False  
+    
     try:
         handler = sqlite3.connect('database/database.db')
         cursor = handler.cursor()
     except sqlite3.Error:
         print("Connection to DB failed.")
-        return False  # Retorna None se a conexão falhar
+        return False  
 
     users = cursor.execute("SELECT user, email FROM users").fetchall()
     handler.close()
@@ -46,9 +45,10 @@ def get_user_email(username: str) -> str:
     for user in users:
         print(user[0])
         if user[0] == username:
-            return user[1]  # Retorna o e-mail associado ao usuário
-
-    return False  # Retorna None se o usuário não for encontrado
+            # Retorna o e-mail associado ao usuário
+            return user[1]  
+    # Caso nada seja encontrado, assume que não existe o user na DB
+    return False  
 
 
 def sender(email: str, password: str, email_body: str, to_email: str) -> bool:
@@ -66,7 +66,7 @@ def sender(email: str, password: str, email_body: str, to_email: str) -> bool:
             return True
             
             
-def request_password_change(username: str) -> bool:
+def send_password_change(username: str):
     """Gera um código de redefinição e envia um e-mail se o usuário existir."""
     email = 'puphealthapp@gmail.com'
     password = 'ujjf oirm izya jhvx'
@@ -100,7 +100,7 @@ Content-Transfer-Encoding: 7bit
 
 {content}
 """
-
+    # "Catcher" se a mensagem foi enviada com sucesso.
     ans = sender(email=email, password=password, email_body=message, to_email=to_addr)
     if ans:
         print('Retornando código e true')
@@ -109,5 +109,28 @@ Content-Transfer-Encoding: 7bit
         return False  # Retorna False se o envio falhar
 
 
-def is_code_valid(input_code, code) -> bool:
-    print(input_code, code)    
+def is_code_valid(input_code: str, code: str) -> bool:
+    return input_code == code
+
+
+def change_password(input_password: str, user: str) -> bool:
+    input_password = input_password.get()
+    print("Password", input_password)
+    
+    if re.match(valid_pass, input_password):
+        try:
+            handler = sqlite3.connect('database/database.db')
+            cursor = handler.cursor()
+            cursor.execute("UPDATE users SET password = ? WHERE user = ?", (input_password,user))
+            handler.commit()
+        except sqlite3.Error as e:
+            print(e)
+            return False
+        else:
+            print("PASSWORD CHANGED")
+        finally:
+            handler.close()
+            return True
+    else:
+        print("NOT VALID")
+        return False
