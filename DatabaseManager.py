99277fcea6ebcb49603164153583edfe3cde9c03
Gabriel <107143364+archgabs@users.noreply.github.com>
2024-11-02 16:08:26 -0300

    Added filter functionality

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -143,7 +143,9 @@ class DashboardUtilities():
     def __init__(self):
         self.handler = sqlite3.connect('database/database.db')
         self.cursor = self.handler.cursor()
-        self.vaccine_pattern = r"^([a-zA-Zá-úÁ-Ú0-9]+)(,\s*[a-zA-Zá-úÁ-Ú0-9]+)*$"
+        # self.vaccine_pattern = r"^([a-zA-Zá-úÁ-Ú0-9]+)(,\s*[a-zA-Zá-úÁ-Ú0-9]+)*$"
+        self.vaccine_pattern = r"^([a-zA-Zá-úÁ-Ú0-9]+(?:\s+[a-zA-Zá-úÁ-Ú0-9]+)*)(,\s*([a-zA-Zá-úÁ-Ú0-9]+(?:\s+[a-zA-Zá-úÁ-Ú0-9]+)*))*$"
+
 
 
     def get_user(self, usr):
@@ -210,22 +212,47 @@ class DashboardUtilities():
                     return "ANIMAL_ALREADY_ASSIGNED"
         return "ID_MISSING"
     
-    def list_patients(self, mode: str = "INIT") -> list:
-        res = None
+    def list_patients(self, mode: str = "INIT", tutor: str = "") -> list:
         match mode:
             case "INIT":
-                res = self.cursor.execute('''
+                return self.cursor.execute('''
     SELECT p.id, p.nome AS paciente_nome, t.nome_tutor AS tutor_nome, p.vacinas_tomadas 
     FROM patients p 
     JOIN tutor t ON p.tutor = t.id 
     ORDER BY p.id
 ''').fetchall()
             case "Tutor":
-                print(":3")
-                res = None
+                return self.cursor.execute('''
+        SELECT p.id, p.nome AS paciente_nome, t.nome_tutor AS tutor_nome, p.vacinas_tomadas 
+        FROM patients p 
+        JOIN tutor t ON p.tutor = t.id 
+        WHERE t.nome_tutor = ? 
+        ORDER BY p.id
+    ''', (tutor,)).fetchall() 
             case "Nome Paciente": 
-                res = None
-                print(":P")
+                return self.cursor.execute('''
+        SELECT p.id, p.nome AS paciente_nome, t.nome_tutor AS tutor_nome, p.vacinas_tomadas 
+        FROM patients p 
+        JOIN tutor t ON p.tutor = t.id 
+        WHERE paciente_nome = ? 
+        ORDER BY p.id
+    ''', (tutor,)).fetchall() 
         
-        return res
-                
+
+    def does_tutor_or_patient_exist(self, nome: str, is_tutor: bool= False):
+        if is_tutor:
+            return self.cursor.execute('SELECT * FROM tutor WHERE nome_tutor = ?', (nome,))
+        else:
+            return self.cursor.execute('SELECT * FROM patients WHERE nome = ?', (nome,))
+    
+    def alter_btns_functionality(self, id: int, mode: str) -> None:
+        match mode: 
+            case "Alterar Nome":
+                print(mode, id)
+            case "Modificar Vacinas":
+                print(mode, id)
+            case "Alterar Gênero":
+                print(mode, id)
+    
+    
+    def close_db(self): self.handler.close()
